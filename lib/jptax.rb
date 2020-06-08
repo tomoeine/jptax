9e2e1cac68bb08bb89f20254be3957eb7b7e3b4a
Tomoe Sawai <tomoe.teshima@gmail.com>
2020-06-08 14:14:58 +0900

    [add] enable to specify round types

--- lib/jptax.rb
+++ lib/jptax.rb
@@ -1,8 +1,14 @@
 require 'jptax/version'
 
 module Jptax
-  def amount_with_tax(amount=0, amount_for_reduced=0)
-    (amount.to_i * 1.1 + amount_for_reduced.to_i * 1.08).floor
+  def amount_with_tax(amount=0, amount_for_reduced=0, round_types='floor')
+    if round_types == 'ceil'
+      (amount.to_i * 1.1 + amount_for_reduced.to_i * 1.08).ceil
+    elsif round_types == 'round'
+      (amount.to_i * 1.1 + amount_for_reduced.to_i * 1.08).round
+    else
+      (amount.to_i * 1.1 + amount_for_reduced.to_i * 1.08).floor
+    end
   end
 
   module_function :amount_with_tax
--- spec/jptax_spec.rb
+++ spec/jptax_spec.rb
@@ -24,12 +24,34 @@ RSpec.describe Jptax do
       end
     end
 
-    context '端数が発生する場合' do
+    context '端数が発生する場合で、丸め方法を指定しなかった場合' do
       let(:amount) { 1000 }
       let(:amount_for_reduced) { 1999 }
 
       it '切り捨てられる' do
         is_expected.to eq 3258
+      end
+    end
+
+    context '端数が発生する場合で、丸め方法を`floor`に指定した場合' do
+      let(:amount) { 1000 }
+      let(:amount_for_reduced) { 1999 }
+
+      it '切り捨てられる' do
+        expect(Jptax.amount_with_tax(amount, amount_for_reduced, 'floor')).to eq 3258
+      end
+    end
+
+    context '端数が発生する場合で、丸め方法を`ceil`に指定した場合' do
+      it '切り上げられる' do
+        expect(Jptax.amount_with_tax(1000, 1999, 'ceil')).to eq 3259
+      end
+    end
+
+    context '端数が発生する場合で、丸め方法を`round`に指定した場合' do
+      it '四捨五入される' do
+        expect(Jptax.amount_with_tax(1000, 1999, 'round')).to eq 3259
+        expect(Jptax.amount_with_tax(1000, 1989, 'round')).to eq 3248
       end
     end
 
